86717952270a58d5a2b208aa179550933b91a058
Leonid Radkevich <laradkevich@gmail.com>
2020-10-08 20:00:30 +0300

    Add project files.

---
+++ day2beer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day2beer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
