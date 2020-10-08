8f427a40a9704477e9906685ba1aa56a9dc24544
Leonid Radkevich <laradkevich@gmail.com>
2020-10-08 20:06:15 +0300

    Added another console output

--- day2beer/Program.cs
+++ day2beer/Program.cs
@@ -7,6 +7,7 @@ namespace day2beer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Commit something to github");
         }
     }
 }
